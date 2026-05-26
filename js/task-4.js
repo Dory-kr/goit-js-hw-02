346bbaf5c5398faa50bdfdae283acaa55aba7310
Dory-kr <rey.mitani@gmail.com>
2026-05-26 22:50:45 +0300

    commit1

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+function getShippingCost(country) {
+    let price;
+    switch (country.toLowerCase()) {
+        case "china":
+            price = 100;
+            break;
+        case "chile":
+            price = 250;
+            break;
+        case "australia":
+            price = 170;
+            break;
+        case "jamaica":
+            price = 120;
+            break;
+        default:
+            return `Sorry, there is no delivery to your country`;
+    }
+}
